f4825ee133512c4431a01d367bbaae97ef1e9e46
caimamba <1453492529@qq.com>
2023-06-17 19:52:09 +0800

    build debug environment for user apps

---
+++ apps/busybox/include/applet_tables.h
@@ -0,0 +1,375 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 119
+#define KNOWN_APPNAME_OFFSETS 4
+
+const uint16_t applet_nameofs[] ALIGN2 = {
+152,
+318,
+500,
+};
+
+const char applet_names[] ALIGN1 = ""
+"[" "\0"
+"[[" "\0"
+"arch" "\0"
+"ash" "\0"
+"base32" "\0"
+"base64" "\0"
+"basename" "\0"
+"cal" "\0"
+"cat" "\0"
+"chgrp" "\0"
+"chmod" "\0"
+"chown" "\0"
+"chroot" "\0"
+"cksum" "\0"
+"clear" "\0"
+"comm" "\0"
+"cp" "\0"
+"cut" "\0"
+"date" "\0"
+"dd" "\0"
+"df" "\0"
+"dirname" "\0"
+"dmesg" "\0"
+"dos2unix" "\0"
+"du" "\0"
+"echo" "\0"
+"env" "\0"
+"expand" "\0"
+"expr" "\0"
+"factor" "\0"
+"false" "\0"
+"find" "\0"
+"fold" "\0"
+"fsync" "\0"
+"grep" "\0"
+"groups" "\0"
+"hd" "\0"
+"head" "\0"
+"hexdump" "\0"
+"hostid" "\0"
+"hwclock" "\0"
+"id" "\0"
+"install" "\0"
+"kill" "\0"
+"less" "\0"
+"link" "\0"
+"ln" "\0"
+"logname" "\0"
+"ls" "\0"
+"man" "\0"
+"md5sum" "\0"
+"mesg" "\0"
+"mim" "\0"
+"mkdir" "\0"
+"mkfifo" "\0"
+"mknod" "\0"
+"mktemp" "\0"
+"more" "\0"
+"mv" "\0"
+"nice" "\0"
+"nl" "\0"
+"nohup" "\0"
+"nologin" "\0"
+"nproc" "\0"
+"od" "\0"
+"paste" "\0"
+"pmap" "\0"
+"printenv" "\0"
+"printf" "\0"
+"ps" "\0"
+"pstree" "\0"
+"pwd" "\0"
+"readlink" "\0"
+"realpath" "\0"
+"rm" "\0"
+"rmdir" "\0"
+"script" "\0"
+"seq" "\0"
+"sh" "\0"
+"sha1sum" "\0"
+"sha256sum" "\0"
+"sha3sum" "\0"
+"sha512sum" "\0"
+"shred" "\0"
+"shuf" "\0"
+"sleep" "\0"
+"sort" "\0"
+"split" "\0"
+"stat" "\0"
+"strings" "\0"
+"stty" "\0"
+"sum" "\0"
+"sync" "\0"
+"tac" "\0"
+"tail" "\0"
+"tee" "\0"
+"test" "\0"
+"time" "\0"
+"timeout" "\0"
+"touch" "\0"
+"tr" "\0"
+"true" "\0"
+"truncate" "\0"
+"tty" "\0"
+"uname" "\0"
+"unexpand" "\0"
+"uniq" "\0"
+"unix2dos" "\0"
+"unlink" "\0"
+"uptime" "\0"
+"usleep" "\0"
+"uudecode" "\0"
+"uuencode" "\0"
+"wc" "\0"
+"which" "\0"
+"whoami" "\0"
+"xargs" "\0"
+"xxd" "\0"
+"yes" "\0"
+;
+
+#define APPLET_NO_arch 2
+#define APPLET_NO_ash 3
+#define APPLET_NO_base32 4
+#define APPLET_NO_base64 5
+#define APPLET_NO_basename 6
+#define APPLET_NO_cal 7
+#define APPLET_NO_cat 8
+#define APPLET_NO_chgrp 9
+#define APPLET_NO_chmod 10
+#define APPLET_NO_chown 11
+#define APPLET_NO_chroot 12
+#define APPLET_NO_cksum 13
+#define APPLET_NO_clear 14
+#define APPLET_NO_comm 15
+#define APPLET_NO_cp 16
+#define APPLET_NO_cut 17
+#define APPLET_NO_date 18
+#define APPLET_NO_dd 19
+#define APPLET_NO_df 20
+#define APPLET_NO_dirname 21
+#define APPLET_NO_dmesg 22
+#define APPLET_NO_dos2unix 23
+#define APPLET_NO_du 24
+#define APPLET_NO_echo 25
+#define APPLET_NO_env 26
+#define APPLET_NO_expand 27
+#define APPLET_NO_expr 28
+#define APPLET_NO_factor 29
+#define APPLET_NO_false 30
+#define APPLET_NO_find 31
+#define APPLET_NO_fold 32
+#define APPLET_NO_fsync 33
+#define APPLET_NO_grep 34
+#define APPLET_NO_groups 35
+#define APPLET_NO_hd 36
+#define APPLET_NO_head 37
+#define APPLET_NO_hexdump 38
+#define APPLET_NO_hostid 39
+#define APPLET_NO_hwclock 40
+#define APPLET_NO_id 41
+#define APPLET_NO_install 42
+#define APPLET_NO_kill 43
+#define APPLET_NO_less 44
+#define APPLET_NO_link 45
+#define APPLET_NO_ln 46
+#define APPLET_NO_logname 47
+#define APPLET_NO_ls 48
+#define APPLET_NO_man 49
+#define APPLET_NO_md5sum 50
+#define APPLET_NO_mesg 51
+#define APPLET_NO_mim 52
+#define APPLET_NO_mkdir 53
+#define APPLET_NO_mkfifo 54
+#define APPLET_NO_mknod 55
+#define APPLET_NO_mktemp 56
+#define APPLET_NO_more 57
+#define APPLET_NO_mv 58
+#define APPLET_NO_nice 59
+#define APPLET_NO_nl 60
+#define APPLET_NO_nohup 61
+#define APPLET_NO_nologin 62
+#define APPLET_NO_nproc 63
+#define APPLET_NO_od 64
+#define APPLET_NO_paste 65
+#define APPLET_NO_pmap 66
+#define APPLET_NO_printenv 67
+#define APPLET_NO_printf 68
+#define APPLET_NO_ps 69
+#define APPLET_NO_pstree 70
+#define APPLET_NO_pwd 71
+#define APPLET_NO_readlink 72
+#define APPLET_NO_realpath 73
+#define APPLET_NO_rm 74
+#define APPLET_NO_rmdir 75
+#define APPLET_NO_script 76
+#define APPLET_NO_seq 77
+#define APPLET_NO_sh 78
+#define APPLET_NO_sha1sum 79
+#define APPLET_NO_sha256sum 80
+#define APPLET_NO_sha3sum 81
+#define APPLET_NO_sha512sum 82
+#define APPLET_NO_shred 83
+#define APPLET_NO_shuf 84
+#define APPLET_NO_sleep 85
+#define APPLET_NO_sort 86
+#define APPLET_NO_split 87
+#define APPLET_NO_stat 88
+#define APPLET_NO_strings 89
+#define APPLET_NO_stty 90
+#define APPLET_NO_sum 91
+#define APPLET_NO_sync 92
+#define APPLET_NO_tac 93
+#define APPLET_NO_tail 94
+#define APPLET_NO_tee 95
+#define APPLET_NO_test 96
+#define APPLET_NO_time 97
+#define APPLET_NO_timeout 98
+#define APPLET_NO_touch 99
+#define APPLET_NO_tr 100
+#define APPLET_NO_true 101
+#define APPLET_NO_truncate 102
+#define APPLET_NO_tty 103
+#define APPLET_NO_uname 104
+#define APPLET_NO_unexpand 105
+#define APPLET_NO_uniq 106
+#define APPLET_NO_unix2dos 107
+#define APPLET_NO_unlink 108
+#define APPLET_NO_uptime 109
+#define APPLET_NO_usleep 110
+#define APPLET_NO_uudecode 111
+#define APPLET_NO_uuencode 112
+#define APPLET_NO_wc 113
+#define APPLET_NO_which 114
+#define APPLET_NO_whoami 115
+#define APPLET_NO_xargs 116
+#define APPLET_NO_xxd 117
+#define APPLET_NO_yes 118
+
+#ifndef SKIP_applet_main
+int (*const applet_main[])(int argc, char **argv) = {
+test_main,
+test_main,
+uname_main,
+ash_main,
+baseNUM_main,
+baseNUM_main,
+basename_main,
+cal_main,
+cat_main,
+chgrp_main,
+chmod_main,
+chown_main,
+chroot_main,
+cksum_main,
+clear_main,
+comm_main,
+cp_main,
+cut_main,
+date_main,
+dd_main,
+df_main,
+dirname_main,
+dmesg_main,
+dos2unix_main,
+du_main,
+echo_main,
+env_main,
+expand_main,
+expr_main,
+factor_main,
+false_main,
+find_main,
+fold_main,
+fsync_main,
+grep_main,
+id_main,
+hexdump_main,
+head_main,
+hexdump_main,
+hostid_main,
+hwclock_main,
+id_main,
+install_main,
+kill_main,
+less_main,
+link_main,
+ln_main,
+logname_main,
+ls_main,
+man_main,
+md5_sha1_sum_main,
+mesg_main,
+scripted_main,
+mkdir_main,
+mkfifo_main,
+mknod_main,
+mktemp_main,
+more_main,
+mv_main,
+nice_main,
+nl_main,
+nohup_main,
+scripted_main,
+nproc_main,
+od_main,
+paste_main,
+pmap_main,
+printenv_main,
+printf_main,
+ps_main,
+pstree_main,
+pwd_main,
+readlink_main,
+realpath_main,
+rm_main,
+rmdir_main,
+script_main,
+seq_main,
+ash_main,
+md5_sha1_sum_main,
+md5_sha1_sum_main,
+md5_sha1_sum_main,
+md5_sha1_sum_main,
+shred_main,
+shuf_main,
+sleep_main,
+sort_main,
+split_main,
+stat_main,
+strings_main,
+stty_main,
+sum_main,
+sync_main,
+tac_main,
+tail_main,
+tee_main,
+test_main,
+time_main,
+timeout_main,
+touch_main,
+tr_main,
+true_main,
+truncate_main,
+tty_main,
+uname_main,
+expand_main,
+uniq_main,
+dos2unix_main,
+unlink_main,
+uptime_main,
+usleep_main,
+uudecode_main,
+uuencode_main,
+wc_main,
+which_main,
+whoami_main,
+xargs_main,
+xxd_main,
+yes_main,
+};
+#endif
+
