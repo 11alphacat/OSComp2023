720102061f921f8e94188617d3a4180e3d6f4f98
BoneInscri <20051124@hdu.edu.cn>
2023-06-29 20:41:43 +0800

    sdcard and uart init

---
+++ include/driver/uart_hifive.h
@@ -0,0 +1,7 @@
+#ifndef __UART_HIFIVE_H__
+#define __UART_HIFIVE_H__
+
+// init
+
+
+#endif
---
+++ src/driver/sdcard.c
@@ -0,0 +1,11 @@
+#include "driver/sdcard.h"
+
+
+// init
+int sdcard_init() {
+    return 0;
+}
+
+// read and write
+
+
